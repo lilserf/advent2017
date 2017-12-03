1f44b60fd6b5ab5ab41bcae2f79d39e7bbf59a95
Mark Haines <lilserf@gmail.com>
2017-12-03 13:15:16 -0500

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 use std::time::{Duration, Instant};
 
@@ -64,6 +65,7 @@ fn day1b()
 	println!("Sum was {}", sum);
 }
 
+#[allow(dead_code)]
 fn find_common_divisor(nums: &Vec<u32>) -> u32
 {
 	for (i, el1) in nums.iter().enumerate()
@@ -84,6 +86,7 @@ fn find_common_divisor(nums: &Vec<u32>) -> u32
 	return 0;
 }
 
+#[allow(dead_code)]
 fn day2()
 {
 	let input = get_input("day2.txt");
@@ -119,6 +122,118 @@ fn day2()
 	println!("Part b is {}", sumb);
 }
 
+fn biggest_in_ring(ring:i32) -> i32
+{
+	let size = ring * 8;
+
+	if ring > 0
+	{
+		return biggest_in_ring(ring-1) + size;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+fn coord_for_input(input:i32) -> (i32, i32)
+{
+	let mut ring = 1;
+	
+	while biggest_in_ring(ring) < input
+	{
+		ring += 1;
+	}
+
+	let ring_end = biggest_in_ring(ring);
+	let ring_start = ring_end - (ring * 8) + 1;
+	let ring_side = ring * 2 + 1;
+
+	let index_in_ring = input - ring_start;
+	
+	//println!("Input is in ring {}, which runs from {} to {}, and is {}x{}. Input is at index {}", ring, ring_start, ring_end, ring_side, ring_side, index_in_ring);
+
+	let half_side = ring_side / 2;
+	let mut coord = (half_side, half_side-1);
+
+	let mut direction = (0, -1);
+
+	for _i in 0..index_in_ring
+	{
+		coord.0 += direction.0;
+		coord.1 += direction.1;
+		if direction == (0, -1) && coord.1 == -1*half_side
+		{
+			direction = (-1, 0);
+		}
+		if direction == (-1, 0) && coord.0 == -1*half_side
+		{
+			direction = (0, 1);
+		}
+		if direction == (0, 1) && coord.1 == half_side
+		{
+			direction = (1, 0);
+		}
+	}
+
+	return coord;
+}
+
+fn add_up_value(h:&HashMap<(i32,i32), i32>, coord:(i32,i32)) -> i32
+{
+	let neighbors: [(i32, i32); 8] = 
+	[
+		(-1,-1), (0,-1), (1, -1),
+		(-1, 0),         (1, 0),
+		(-1, 1), (0, 1), (1, 1)
+	];
+
+	let mut value = 0;
+	
+	for n in neighbors.iter()
+	{
+		let c = ( coord.0+n.0, coord.1+n.1 );
+		value += match h.get(&c)
+		{
+			Some(v) => *v,
+			None => 0
+		}
+	}
+
+	return value;
+}
+
+fn day3()
+{
+	let input = 312051;
+	//let input = 23;
+	let coord = coord_for_input(input);
+
+	println!("Input is at coord {:?} which is {} steps", coord, coord.0.abs() + coord.1.abs());
+
+	let mut h = HashMap::new();
+
+	// Populate the middle
+	h.insert( (0,0), 1);
+	let mut i = 2;
+	loop
+	{
+		let coord = coord_for_input(i);
+
+		let value = add_up_value(&h, coord);
+		
+		h.insert(coord, value);
+
+		if value > input
+		{
+			println!("First value greater than {} was {} at index {}", input, value, i);
+			break;
+		}
+
+		i += 1;
+	}
+}
+
 fn get_input(name:&str) -> String
 {
 	let prefix = String::from("input/");
@@ -141,7 +256,8 @@ fn main()
 	let now = Instant::now();
     //day1();
 	//day1b();
-	day2();
+	//day2();
+	day3();
 
 	println!("Elapsed: {} ms", as_msecs(now.elapsed()));
 }
